a290651ce6fe8d4ea8602cdc5f0aae0377af41a1
kmwebnet <46954791+kmwebnet@users.noreply.github.com>
2020-11-05 09:31:23 +0900

    rebuild js

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,53 +1,44 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import routes from '../constants/routes.json';
 import NavBar from './NavBar';
 import Home from './components/Home';
 import SubComponent from './components/sub-component';
 import SubComponent2 from './components/sub-component2';
 
-interface State {
-  open: boolean;
-};
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBarSpacer: theme.mixins.toolbar,
+  })
+);
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props {};
 
-class Routes extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
+function Routes () {
+  const [open, setopen] = React.useState<boolean>(false);
 
-		this.handleToggle = this.handleToggle.bind(this) as ((
-			open: boolean,
-		) => boolean);
-		this.state = {
-			open: false,
-		};
+  const handleToggle = React.useCallback(() => {
+    setopen(open => !open);
+  }, []);
 
-  }
+  const classes = useStyles();
 
-	/** If drawer is open, close it. If closed, open it. */
-	public handleToggle(open = !this.state.open): boolean {
-		this.setState({
-			open,
-		});
-		return open;
-	}
-
-  render() {
     return (
-      <Router basename='/app'>
-      <NavBar show={this.state.open} drawToggleClickHandler={this.handleToggle}
+      <Router>
+
+      <NavBar show={open} drawToggleClickHandler={handleToggle}
       />
+        <div className={classes.appBarSpacer}>
         <Switch>
           <Route exact path={routes.HOME} component={Home} />
           <Route path={routes.sub} render={ () => <SubComponent/> } />
           <Route path={routes.sub2} render={ () => <SubComponent2/> } />
         </Switch>
+        </div>
       </Router>
     );
-  };
+
 };
 
 export default Routes;
